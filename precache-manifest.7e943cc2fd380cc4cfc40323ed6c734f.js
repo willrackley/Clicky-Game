579a7ea78b41e883abbf028438d87de1c1a52b07
williamrackley <willrackley65@gmail.com>
2019-04-02 20:03:03 -0400

    Updates

---
+++ precache-manifest.7e943cc2fd380cc4cfc40323ed6c734f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d64dfc8b6cc02fccf127",
+    "url": "/Clicky-Game/static/css/main.29261887.chunk.css"
+  },
+  {
+    "revision": "d64dfc8b6cc02fccf127",
+    "url": "/Clicky-Game/static/js/main.aba037ef.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "a2e9b7c1a25043babe15db454814857c",
+    "url": "/Clicky-Game/static/media/tic-tac-toe.a2e9b7c1.png"
+  },
+  {
+    "revision": "f30bdaf015ba0d450c1a8a55d670c695",
+    "url": "/Clicky-Game/index.html"
+  }
+];
